6b60e7fb913b3fd760e5ca3d1df0b9184d1641c4
Gerd <gerd@niemetz.it>
2016-12-26 21:18:05 +0100

    first commit

---
+++ automount.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+
+
+#/Users/${UserName}/Library/LaunchAgents/it.niemetz.automount.plist
+#<?xml version="1.0" encoding="UTF-8"?>
+#<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+#<plist version="1.0">
+#<dict>
+#	<key>Label</key>
+#	<string>it.niemetz.automount</string>
+#	<key>ProgramArguments</key>
+#	<array>
+#		<string>/Users/Shared/automount.sh</string>
+#	</array>
+#	<key>RunAtLoad</key>
+#	<true/>
+#</dict>
+#</plist>
+#launchctl load /Users/${UserName}/Library/LaunchAgents/it.niemetz.automount.plist
+
+#/Users/${UserName}/Library/Preferences/it.niemetz.automount.plist
+#<?xml version="1.0" encoding="UTF-8"?>
+#<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+#<plist version="1.0">
+#<dict>
+#	<key>Sharelist</key>
+#	<array>
+#		<dict>
+#			<key>acct</key>
+#			<string>ACCOUNT</string>
+#			<key>ptcl</key>
+#			<string>PROTOCOL (afp/smb)</string>
+#			<key>shre</key>
+#			<string>SHARE</string>
+#			<key>srvr</key>
+#			<string>SERVER</string>
+#		</dict>
+#	</array>
+#</dict>
+#</plist>
+
+#security add-internet-password \
+#  -a ACCOUNT \
+#  -l LABEL (eg. same as SERVER) \
+#  -D DESCRIPTION (eg. Networkpassword) \
+#  -r PROTOCOL ("afp "/"smb ") \
+#  -s SERVER \
+#  -w PASSWORD \
+#  -T /usr/bin/security \
+#  -T /System/Library/CoreServices/NetAuthAgent.app/Contents/MacOS/NetAuthSysAgent \
+#  -T /System/Library/CoreServices/NetAuthAgent.app \
+#  -T group://NetAuth \
+#  /Users/${UserName}/Library/Keychains/login.keychain
+
+UserName="$(logname)"
+AutomountPL="/Users/${UserName}/Library/Preferences/it.niemetz.automount.plist"
+LoginKC="/Users/${UserName}/Library/Keychains/login.keychain"
+declare -i Idx=0
+declare -i Retry
+declare -i MaxRetry=30
+
+function cleanup {
+  unset KCpassword
+  exit ${1}
+}
+
+trap 'cleanup' SIGHUP SIGINT SIGQUIT SIGTERM EXIT
+
+if [[ -s "${AutomountPL}" ]] && [[ -s "${LoginKC}" ]]; then
+  while /usr/libexec/PlistBuddy -c "Print Sharelist:${Idx}" "${AutomountPL}" >/dev/null 2>&1; do
+    PLptcl="$(/usr/libexec/PlistBuddy -c "Print Sharelist:${Idx}:ptcl" "${AutomountPL}" 2>/dev/null)"
+    PLacct="$(/usr/libexec/PlistBuddy -c "Print Sharelist:${Idx}:acct" "${AutomountPL}" 2>/dev/null)"
+    PLacct="${PLacct:-${UserName}}"
+    PLsrvr="$(/usr/libexec/PlistBuddy -c "Print Sharelist:${Idx}:srvr" "${AutomountPL}" 2>/dev/null)"
+    PLshre="$(/usr/libexec/PlistBuddy -c "Print Sharelist:${Idx}:shre" "${AutomountPL}" 2>/dev/null)"
+    if [[ -n "${PLptcl}" && -n "${PLacct}" && -n "${PLsrvr}" && -n "${PLshre}" ]] && ! mount | egrep -s -q "^//${PLacct}@${PLsrvr}/${PLshre} on /Volumes/${PLshre} \(${PLptcl}fs,.*${UserName}\)$" 2>/dev/null; then
+      Retry=0
+      while ! ping -c 1 -t 1 -o -q "${PLsrvr}" >/dev/null 2>&1 && [[ ${Retry} -le ${MaxRetry} ]]; do
+        if [[ ${Retry} -le ${MaxRetry} ]]; then
+          sleep 1
+          ((Retry++))
+        fi
+      done
+      if [[ ${Retry} -ge ${MaxRetry} ]]; then
+        ((Idx++))
+        continue
+      fi
+
+      if ! eval $(
+      security find-internet-password \
+        -g \
+        -r "$(printf "%-4s" "${PLptcl}")" \
+        -a "${PLacct}" \
+        -l "${PLsrvr}" \
+        "${LoginKC}" 2>&1 |\
+      awk '
+      /password:/ {
+        split($0, val, /: "/)
+        val[2]=substr(val[2], 1, length(val[2])-1)
+        gsub(/"/, "\\\"", val[2])
+        printf("KC%s=\"%s\"\n", val[1], val[2])
+      }
+      '
+      ); then
+        ((Idx++))
+        continue
+      fi
+    
+      if [[ ! -d "/Volumes/${PLshre}" ]] && ! { mkdir -p "/Volumes/${PLshre}" && chown "${UserName}:staff" "/Volumes/${PLshre}"; } >/dev/null 2>&1;  then
+        rmdir "/Volumes/${PLshre}" >/dev/null 2>&1
+        ((Idx++))
+        continue
+      fi
+    
+      mount_afp "${PLptcl}://${PLacct}:${KCpassword}@${PLsrvr}/${PLshre}" "/Volumes/${PLshre}" 2>/dev/null
+    fi
+    ((Idx++))
+  done
+else
+  exit 1
+fi
