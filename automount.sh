861119de8eed95704c4dbeefbb9b023a2aad00ed
Gerd <gerd.niemetz@gmail.com>
2017-02-01 16:12:12 +0100

    minor improvements

--- automount.sh
+++ automount.sh
@@ -4,82 +4,17 @@ if [ "${DEBUG}" == "false" ]; then
 	set +xv
 	ExpectDebug="log_user 0"
 else
+	PS4='+(${BASH_SOURCE:-}:${LINENO:-}): ${FUNCNAME[0]:+${FUNCNAME[0]:-}(): }'
 	set -xv
 	ExpectDebug="log_user 1"
 fi
-
-#${USERHOME}/Library/LaunchAgents/it.niemetz.automount.plist
-#<?xml version="1.0" encoding="UTF-8"?>
-#<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-#<plist version="1.0">
-#<dict>
-#	<key>Label</key>
-#	<string>it.niemetz.automount</string>
-#	<key>ProgramArguments</key>
-#	<array>
-#		<string>/usr/local/bin/automount.sh</string>
-#		<string>--mountall</string>
-#	</array>
-#	<key>RunAtLoad</key>
-#	<true/>
-#	<key>WatchPaths</key>
-#	<array>
-#		<string>/Library/Preferences/SystemConfiguration</string>
-#	</array>
-#	<key>KeepAlive</key>
-#	<dict>
-#		<key>NetworkState</key>
-#		<true/>
-#	</dict>
-#</dict>
-#</plist>
-#chown ${USERNAME}:staff ${USERHOME}/Library/LaunchAgents/it.niemetz.automount.plist
-#chmod 644 ${USERHOME}/Library/LaunchAgents/it.niemetz.automount.plist
-#launchctl load ${USERHOME}/Library/LaunchAgents/it.niemetz.automount.plist
-
-#${USERHOME}/Library/Preferences/it.niemetz.automount.plist
-#<?xml version="1.0" encoding="UTF-8"?>
-#<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-#<plist version="1.0">
-#<dict>
-#	<key>CommonMaxRetryInSeconds</key>
-#	<integer>COMMONMAXRETRYINSECONDS (eg. 30)</integer>
-#	<key>CommonValidIPRanges</key>
-#	<string>COMMONVALIDIPRANGES (eg. 10.0.0,192.168.0)</string>
-#	<key>CommonAccount</key>
-#	<string>COMMONACCOUNT</string>
-#	<key>CommonMountOptions</key>
-#	<string>COMMONMOUNTOPTIONS (nodev,nosuid)</string>
-#	<key>Mountlist</key>
-#	<array>
-#		<dict>
-#			<key>MaxRetryInSeconds</key>
-#			<integer>MAXRETRYINSECONDS</integer>
-#			<key>ValidIPRanges</key>
-#			<string>VALIDIPRANGES (eg. 10.0.0,192.168.0)</string>
-#			<key>MountOptions</key>
-#			<string>MOUNTOPTIONS</string>
-#			<key>Protocol</key>
-#			<string>PROTOCOL (afp/smb/ftp (readonly)/nfs/http/https)</string>
-#			<key>Account</key>
-#			<string>ACCOUNT</string>
-#			<key>Server</key>
-#			<string>SERVER</string>
-#			<key>Share</key>
-#			<string>SHARE</string>
-#		</dict>
-#	</array>
-#</dict>
-#</plist>
-#chown ${USERNAME}:staff ${USERHOME}/Library/Preferences/it.niemetz.automount.plist
-#chmod 644 ${USERHOME}/Library/Preferences/it.niemetz.automount.plist
 
 #security add-internet-password \
 #	-a ACCOUNT \
 #	-l LABEL (same as SERVER) \
 #	-D DESCRIPTION (eg. Networkpassword) \
 #	-j COMMENT (${SCRIPTNAME}) \
-#	-r PROTOCOL ("afp "/"smb "/"ftp"/"htps"/"http") \
+#	-r PROTOCOL ("afp "/"cifs"/"ftp "/"http"/"htps"/"smb ") \
 #	-s SERVER \
 #	-w PASSWORD \
 #	-U \
@@ -88,25 +23,28 @@ fi
 #	-T /System/Library/CoreServices/NetAuthAgent.app/Contents/MacOS/NetAuthSysAgent \
 #	-T /System/Library/CoreServices/NetAuthAgent.app \
 #	-T group://NetAuth \
-#	${USERHOME}/Library/Keychains/login.keychain
+#	${LOGINHOME}/Library/Keychains/login.keychain
 
-#Server="SERVER"; Label="${Server}"; Description="DESCRIPTION"; Protocol="PROTOCOL"; Account="$(id -p | awk '/^login/ { print $2; exit } /^uid/ { print $2 }')"; Userhome="$(dscl . read /Users/${Account} NFSHomeDirectory | cut -d' ' -f2-)"; security add-internet-password -a "${Account}" -l "${Label}" -D "${Description:-Netzwerkpasswort}" -j "automount" -r "$(printf "%-4s" ${Protocol})" -s "${Server}" -w "$(read -p "Password: " -s && echo "${REPLY}")" -U -T /usr/bin/security -T /System/Library/CoreServices/NetAuthAgent.app/Contents/MacOS/NetAuthSysAgent -T /System/Library/CoreServices/NetAuthAgent.app -T group://NetAuth ${USERHOME}/Library/Keychains/login.keychain
-# when error "User interaction is not allowed." occurs, unlock keychain with
-#security unlock-keychain -p "USERNAME_PASSWORD" ${USERHOME}/Library/Keychains/login.keychain
+#Server="SERVER"; Label="${Server}"; Description="DESCRIPTION"; Protocol="PROTOCOL"; Account="$(id -p | awk '/^login/ { print $2; exit } /^uid/ { print $2 }')"; UserHomeDirectory="$(dscl . read /Users/${Account} NFSHomeDirectory | cut -d' ' -f2-)"; security add-internet-password -a "${Account}" -l "${Label}" -D "${Description:-Netzwerkpasswort}" -j "automount" -r "$(printf "%-4s" ${Protocol})" -s "${Server}" -w "$(read -p "Password: " -s && echo "${REPLY}")" -U -T /usr/bin/security -T /System/Library/CoreServices/NetAuthAgent.app/Contents/MacOS/NetAuthSysAgent -T /System/Library/CoreServices/NetAuthAgent.app -T group://NetAuth ${UserHomeDirectory}/Library/Keychains/login.keychain
+# when error "User interaction is not allowed." occurs, unlock keychain
+# RC=36 (security error 36 -> Error: 0x00000024 36 CSSM_ERRCODE_OBJECT_ACL_REQUIRED)
+#security unlock-keychain -p "LOGINNAME_PASSWORD" ${LOGINHOME}/Library/Keychains/login.keychain
 
 #/usr/local/bin/automount.sh
 #chown root:admin /usr/local/bin/automount.sh
 #chmod 755 /usr/local/bin/automount.sh
 
 # CONSTANTS
-declare -r SCRIPTLASTMOD="2017-01-31"
-declare -r SCRIPTVERSION="0.91"
+declare -r SCRIPTLASTMOD="2017-02-01"
+declare -r SCRIPTVERSION="0.90.1"
 declare -ri YES=0
 declare -ri SUCCESS=${YES}
 declare -ri TRUE=${YES}
+declare -ri FOUND=${YES}
 declare -ri NO=1
 declare -ri ERROR=${NO}
 declare -ri FALSE=${NO}
+declare -ri MISSING=${NO}
 # script path name
 SCRIPT_PN="${0%/*}"
 if [ "${SCRIPT_PN}" == "." ]; then
@@ -129,6 +67,8 @@ declare -r SCRIPT_PN SCRIPTNAME SCRIPTEXTENSION
 declare -r USERNAME="$(id -p | awk '/^uid/ { print $2 }')"
 # user id
 declare -r USERID="$(dscl . read /Users/${USERNAME} UniqueID | awk -F': ' '{ print $2 }')"
+# user primary group id
+declare -r USERPRIMARYGROUPID="$(dscl . read /Users/${USERNAME} PrimaryGroupID | awk -F': ' '{ print $2 }')"
 # user home
 declare -r USERHOME="$(dscl . read /Users/${USERNAME} NFSHomeDirectory | awk -F': ' '{ print $2 }')"
 # login name
@@ -137,33 +77,27 @@ if [ -z "${LOGINNAME}" ]; then
 	LOGINNAME="${USERNAME}"
 	# login id
 	LOGINID="${USERID}"
+	# login primary group id
+	LOGINPRIMARYGROUPID=${USERPRIMARYGROUPID}
 	# login home
 	LOGINHOME="${USERHOME}"
 	# launch as user
 	LAUNCHASUSER=""
 else
 	LOGINID="$(dscl . read /Users/${LOGINNAME} UniqueID | awk -F': ' '{ print $2 }')"
+	LOGINPRIMARYGROUPID="$(dscl . read /Users/${LOGINNAME} PrimaryGroupID | awk -F': ' '{ print $2 }')"
 	LOGINHOME="$(dscl . read /Users/${LOGINNAME} NFSHomeDirectory | awk -F': ' '{ print $2 }')"
-	LAUNCHASUSER="launchctl asuser ${LOGINID}"
+	LAUNCHASUSER="launchctl asuser ${LOGINID} chroot -u ${LOGINID} -g ${LOGINPRIMARYGROUPID} /"
 fi
-declare -r LOGINNAME LOGINID LOGINHOME LAUNCHASUSER
+declare -r LOGINNAME LOGINID LOGINPRIMARYGROUPID LOGINHOME LAUNCHASUSER
 # case $(ps -o state= -p ${$}) in
 if [ -t 0 ]; then
 		# interactive shell (not started from launch daemon)
-		declare -ri INTERACTIVE=${YES}
-		# Log the message to standard error
-		declare -r LOGGEROPTION="-s"
+		declare -ri BACKGROUND=${NO}
 else
 		# background shell
-		declare -ri INTERACTIVE=${NO}
-		declare -r LOGGEROPTION=""
+		declare -ri BACKGROUND=${YES}
 fi
-:<<EOS
-if RV="$(pgrep -f -l "${SCRIPT_FN}")"; then
-	log "${SCRIPT_FN} is already running, RV=${RV}"
-	exit 1
-fi
-EOS
 # log dir (absolute path name)
 declare -r LOG_APN="${LOGINHOME}/Library/Logs"
 # log file (absolute file name)
@@ -175,7 +109,6 @@ declare -r LOCK_APN="${TMP_APN}/${SCRIPTNAME}.lock"
 # lock file (absolute file name)
 declare -r LOCK_AFN="${LOCK_APN}/pid"
 # log levels
-#"Emergency" "Alert" "Critical" "Error" "Warning" "Notice" "Info" "Debug"
 declare -a LOG_LEVEL
 declare -r LOG_EMERGENCY=${#LOG_LEVEL[@]}
 LOG_LEVEL[${#LOG_LEVEL[@]}]="Emergency"
@@ -195,15 +128,15 @@ declare -r LOG_DEBUG=${#LOG_LEVEL[@]}
 LOG_LEVEL[${#LOG_LEVEL[@]}]="Debug"
 declare -r LOG_LEVEL
 # automount plist (absolute file name)
-declare -r AUTOMOUNTPLIST_AFN="${USERHOME}/Library/Preferences/it.niemetz.automount.plist"
+declare -r AUTOMOUNTPLIST_AFN="${LOGINHOME}/Library/Preferences/it.niemetz.automount.plist"
 # login keychain (absolute file name)
-declare -r LOGINKEYCHAIN_AFN="${USERHOME}/Library/Keychains/login.keychain"
+declare -r LOGINKEYCHAIN_AFN="${LOGINHOME}/Library/Keychains/login.keychain"
 # max pings
 declare -ir MAXRETRYINSECONDS=30
 # mount options
 declare -r MOUNTOPTIONS="nodev,nosuid"
 # map protocol to value in keychain
-declare -ra PROTOCOLMAPPING=( 'afp="afp "' 'cifs="cifs "' 'ftp="ftp "' 'http="http"' 'https="htps"' 'smb="smb "' )
+declare -ra PROTOCOLMAPPING=( 'afp="afp "' 'cifs="cifs"' 'ftp="ftp "' 'http="http"' 'https="htps"' 'smb="smb "' )
 
 # Global variables
 # index counter
@@ -211,9 +144,9 @@ declare -i Idx=0
 # retry counter
 declare -i Try
 # is ip in valid range
-declare -i IsInValidRange=0
+declare -i IsInValidRange=${TRUE}
 # exit code
-declare -i EC=0
+declare -i EC=${TRUE}
 # array of ip addresses
 declare -a IPAddresses=()
 # late bound variables
@@ -333,9 +266,9 @@ function getIPAddresses {
 
 	if [ -n "${_IPAddresses}" ]; then
 		echo "${_IPAddresses}"
-		return 0
+		return ${TRUE}
 	else
-		return 1
+		return ${FALSE}
 	fi
 }
 
@@ -359,14 +292,21 @@ function getKeychainProtocol {
 }
 
 function getPasswordFromKeychain {
+	local -i _RC=${TRUE}
+
 	security find-internet-password \
 		-w \
 		-r "$(getKeychainProtocol)" \
 		-a "${Account}" \
 		-l "${Server}" \
 		-j "${SCRIPTNAME}" \
-		"${LOGINKEYCHAIN_AFN}" 2>/dev/null
-	return ${?}
+		"${LOGINKEYCHAIN_AFN}" 2>&1
+	_RC=${?}
+
+	if [ ${_RC} -ne ${SUCCESS} ]; then
+		log --priority=${LOG_ERROR} "getPasswordFromKeychain failed (RC=${_RC}, RV=${_RV})"
+	fi
+	return ${_RC}
 }
 
 function mountAll {
@@ -390,13 +330,14 @@ function mountAll {
 			Account="${Account:-${CommonAccount}}"
 			Server="$(/usr/libexec/PlistBuddy -c "Print Mountlist:${Idx}:Server" "${AUTOMOUNTPLIST_AFN}" 2>/dev/null)"
 			Share="$(/usr/libexec/PlistBuddy -c "Print Mountlist:${Idx}:Share" "${AUTOMOUNTPLIST_AFN}" 2>/dev/null)"
-			if [[ -n "${Protocol}" && -n "${Account}" && -n "${Server}" && -n "${Share}" ]] && ! mount | egrep -s -q "//.*${Server}/(${Share})? on /Volumes/${Share} \(.*, mounted by ${USERNAME}\)$" 2>/dev/null; then
+			if [[ -n "${Protocol}" && -n "${Account}" && -n "${Server}" && -n "${Share}" ]] && ! mount | egrep -s -q "//.*${Server}/(${Share})? on /Volumes/${Share} \(.*, mounted by ${LOGINNAME}\)$" 2>/dev/null; then
+				IsInValidRange=${TRUE}
 				if [ -n "${ValidIPRanges}" ]; then
-					IsInValidRange=1
+					IsInValidRange=${FALSE}
 					for IPAddress in "${IPAddresses[@]}"; do
 						IPAddressPart="$(echo "${IPAddress}" | cut -d'.' -f1-3)"
 						if [[ "${ValidIPRanges}" =~ (^|,)"${IPAddressPart}"(,|$) ]]; then
-							IsInValidRange=0
+							IsInValidRange=${TRUE}
 							break
 						fi
 					done
@@ -404,17 +345,21 @@ function mountAll {
 
 				if [ ${IsInValidRange} -eq ${TRUE} ]; then
 					Try=0
-					while ! ping -c 1 -t 1 -o -q "${Server}" >/dev/null 2>&1 && [ ${Try} -le ${MaxRetryInSeconds} ]; do
+					while ! RV="$(ping -c 1 -t 1 -o -q "${Server}" 2>&1)" && [ ${Try} -le ${MaxRetryInSeconds} ]; do
 						((Try++))
 					done
 					if [ ${Try} -gt ${MaxRetryInSeconds} ]; then
+						log --priority=${LOG_ERROR} "Could not ping ${Server} within ${MaxRetryInSeconds} (RC=${RC}, RV=${RV})"
 						((Idx++))
 						continue
 					fi
 						
 					MountPoint="${Share##*/}"
 					if [ ! -d "/Volumes/${MountPoint}" ]; then
-						if ! { mkdir -p ${Verbose} "/Volumes/${MountPoint}" && chown "${USERNAME}:staff" "/Volumes/${MountPoint}"; }; then
+						RV="$( { mkdir -p ${Verbose} "/Volumes/${MountPoint}" && chown "${LOGINNAME}:${LOGINPRIMARYGROUPID}" "/Volumes/${MountPoint}"; } 2>&1 )"
+						RC=${?}
+						if [ ${RC} -ne ${SUCCESS} ]; then
+							log --priority=${LOG_ERROR} "Could not create \"/Volumes/${MountPoint}\" (RC=${RC}, RV=${RV})"
 							rmdir "/Volumes/${MountPoint}" >/dev/null 2>&1
 							((Idx++))
 							continue
@@ -477,18 +422,20 @@ function mountAll {
 							RC=${?}
 							;;
 						*)
-							log -p ${LOG_ERROR} "Unknown protocol ${Protocol}"
+							log --priority=${LOG_ERROR} "Unknown protocol ${Protocol}"
 							((Idx++))
 							continue
 							;;
 					esac
 					if [ ${RC} -eq ${SUCCESS} ]; then
-						log -p ${LOG_INFO} "${Share} mounted successfully"
+						log --priority=${LOG_INFO} "${Share} mounted successfully"
 						((MountedShares++))
 					else
-						log -p ${LOG_ERROR} "mount of ${Share} failed (RC=${RC}, RV=${RV})"
+						log --priority=${LOG_ERROR} "mount of ${Share} failed (RC=${RC}, RV=${RV})"
 					fi
 					EC=$((EC||RC))
+				else
+					log --priority=${LOG_ERROR} "Could not create \"/Volumes/${MountPoint}\" (RC=${RC}, RV=${RV})"
 				fi
 			fi
 			((Idx++))
@@ -499,14 +446,14 @@ function mountAll {
 	fi
 	if [ ${EC} -eq ${SUCCESS} ]; then
 		if [ ${MountedShares} -eq ${Idx} ]; then
-			log -p ${LOG_INFO} "automount runned successfully."
+			log --priority=${LOG_INFO} "automount runned successfully."
 		fi
-		if [ ${INTERACTIVE} -eq ${NO} ]; then
+		if [ ${BACKGROUND} -eq ${YES} ]; then
 			${LAUNCHASUSER} /usr/bin/osascript -e "display notification \"automount runned successfully.\" with title \"automount\" subtitle \"\""
 		fi
 	else
-		log -p ${LOG_ERROR} "automount runned with errors."
-		if [ ${INTERACTIVE} -eq ${NO} ]; then
+		log --priority=${LOG_ERROR} "automount runned with errors."
+		if [ ${BACKGROUND} -eq ${YES} ]; then
 			${LAUNCHASUSER} /usr/bin/osascript -e "display notification \"automount runned with errors.\" with title \"automount\" subtitle \"\""
 		fi
 	fi
@@ -516,13 +463,15 @@ function mountAll {
 
 function addPassword {
 	local _Account="${Account:-${LOGINNAME}}"
-	local _Userhome="$(dscl . read /Users/${_Account} NFSHomeDirectory | cut -d' ' -f2-)"
+	local _AccountHomeDirectory="$(dscl . read /Users/${_Account} NFSHomeDirectory | cut -d' ' -f2-)"
 	local _AppAccess=""
+	local _RV=""
+	local -i RC=0
 
 	if [[ "${Protocol}" =~ ^http(s)+ ]]; then
 		_AppAccess="-T /System/Library/Extensions/webdav_fs.kext/Contents/Resources/webdavfs_agent"
 	fi
-	RV="$(security add-internet-password \
+	_RV="$(security add-internet-password \
 		-a "${_Account}" \
 		-l "${Server}" \
 		-D "${Description:-Netzwerkpasswort}" \
@@ -536,26 +485,41 @@ function addPassword {
 		-T /System/Library/CoreServices/NetAuthAgent.app \
 		-T group://NetAuth \
 		${_AppAccess} \
-		"${_Userhome}"/Library/Keychains/login.keychain)"
-	RC=${?}
+		"${_AccountHomeDirectory}"/Library/Keychains/login.keychain 2>&1)"
+	_RC=${?}
 
-	if [ ${RC} -eq ${SUCCESS} ]; then
-		log -p ${LOG_INFO} "successfully added password to keychain."
+	if [ ${_RC} -eq ${SUCCESS} ]; then
+		log --priority=${LOG_INFO} "successfully added password to keychain."
 	else
-		log -p ${LOG_ERROR} "error adding password to keychain. (RC=${RC}, RV=${RV})"
+		log --priority=${LOG_ERROR} "error adding password to keychain. (RC=${_RC}, RV=${_RV})"
 	fi
-	exit ${RC}
+	exit ${_RC}
 }
 
 function create_lock {
-	if ! { mkdir "${LOCK_APN}" && echo "${$}" > "${LOCK_AFN}"; } 2>/dev/null; then
-		_RunningPID="$(cat "${LOCK_AFN}")"
-		if RV="$(pgrep -f -l -F "${LOCK_AFN}" "${SCRIPT_FN}")"; then
-			log -p ${LOG_ERROR} "${SCRIPT_FN} is already running${_RunningPID:+ with PID ${_RunningPID}}"
+	local _RV
+	local -i _RC=${TRUE}
+
+	_RV="$( { mkdir "${LOCK_APN}" && echo "${$}" > "${LOCK_AFN}"; } 2>&1 || false )"
+	_RC=${?}
+	if [ ${_RC} -ne ${SUCCESS} ]; then
+		if [ -s "${LOCK_AFN}" ]; then
+			_RunningPID="$(cat "${LOCK_AFN}")"
+			_RV="$(pgrep -f -l -F "${LOCK_AFN}" "${SCRIPT_FN}" 2>&1)"
+			_RC=${?}
+		else
+			_RunningPID=""
+			_RV="$(pgrep -f -l "${SCRIPT_FN}" 2>&1)"
+			_RC=${?}
+		fi
+		if [ ${_RC} -eq ${FOUND} ]; then
+			log --priority=${LOG_ERROR} "${SCRIPT_FN} is already running${_RunningPID:+ with PID ${_RunningPID}}"
 			exit 1
 		else
-			if ! { rm -rf "${LOCK_APN}" && mkdir "${LOCK_APN}" && echo "${$}" > "${LOCK_AFN}"; } 2>/dev/null; then
-				log -p ${LOG_ERROR} "Could not create \"${LOCK_AFN}\", exiting"
+			_RV="$( { rm -rf "${LOCK_APN}" && mkdir "${LOCK_APN}" && echo "${$}" > "${LOCK_AFN}"; } 2>&1 || false )"
+			_RC=${?}
+			if [ ${_RC} -ne ${SUCCESS} ]; then
+				log --priority=${LOG_ERROR} "Could not create \"${LOCK_AFN}\", exiting|RC=${_RC}|RV=${_RV}"
 				exit 1
 			fi
 		fi
